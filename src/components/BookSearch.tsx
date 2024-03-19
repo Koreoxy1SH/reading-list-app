e561651009ea48db536327799ff478e1989e0b30
Koreoxy <rayssankn@gmail.com>
2024-03-20 03:11:49 +0700

    add: pagination funtion

--- src/components/BookSearch.tsx
+++ src/components/BookSearch.tsx
@@ -2,27 +2,49 @@ import axios from "axios"
 import React, { useState } from "react"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "@/components/ui/table"
+
+export type Book = {
+	key: string
+	title: string
+	author_name: string
+	first_publish_year: string
+	number_of_pages_median: string
+	status: "done" | "inProgress" | "backlog"
+}
 
 export const BookSearch = () => {
 	const [query, setQuery] = useState("")
-	const [results, setResults] = useState<any[]>([])
+	const [results, setResults] = useState<Book[]>([])
 	const [isLoading, setIsLoading] = useState(false)
+	const [totalResults, setTotalResults] = useState(0)
+	const [currentPage, setCurrentPage] = useState(1)
+	const resultsPerPage = 100
 
 	type SearchResult = {
-		docs: any[]
+		docs: Book[]
 		numFound: number
 	}
 
-	const searchBooks = async () => {
+	const searchBooks = async (page: number = 1) => {
 		if (!query) return
 
 		setIsLoading(true)
 		try {
 			const response = await axios.get<SearchResult>(
-				`https://openlibrary.org/search.json?q=${query}`,
+				`https://openlibrary.org/search.json?q=${query}&page=${page}&limit=${resultsPerPage}`,
 			)
 
 			setResults(response.data.docs)
+			setTotalResults(response.data.numFound)
+			setCurrentPage(page)
 		} catch (error) {
 			console.error("Error fetching OpenLibrary API data", error)
 		}
@@ -34,6 +56,21 @@ export const BookSearch = () => {
 			searchBooks()
 		}
 	}
+
+	const handlePreviousClick = () => {
+		if (currentPage > 1) {
+			searchBooks(currentPage - 1)
+		}
+	}
+
+	const handleNextClick = () => {
+		if (currentPage < Math.ceil(totalResults / resultsPerPage)) {
+			searchBooks(currentPage + 1)
+		}
+	}
+
+	const startIndex = (currentPage - 1) * resultsPerPage + 1
+	const endIndex = Math.min(startIndex + resultsPerPage - 1, totalResults)
 
 	return (
 		<div className="p-4">
@@ -50,12 +87,55 @@ export const BookSearch = () => {
 				{isLoading ? "Searching..." : "Search"}
 			</Button>
 
+			<div className="mt-2">
+				{totalResults > 0 && (
+					<p className="text-sm">
+						{" "}
+						Showing {startIndex} - {endIndex} out of {totalResults} results
+					</p>
+				)}
+			</div>
+
 			<div className="mt-4 max-h-64 overflow-auto">
-				<ul>
-					{results.map((book, index) => (
-						<li key={index}>{JSON.stringify(book, null, 2)}</li>
-					))}
-				</ul>
+				<Table>
+					<TableHeader>
+						<TableRow>
+							<TableHead className="p-2">Tittle</TableHead>
+							<TableHead className="p-2">Author</TableHead>
+							<TableHead className="p-2">Year</TableHead>
+							<TableHead className="p-2">Page Count</TableHead>
+						</TableRow>
+					</TableHeader>
+					<TableBody>
+						{results.map((book, index) => (
+							<TableRow key={index}>
+								<TableCell>{book.title}</TableCell>
+								<TableCell>{book.author_name}</TableCell>
+								<TableCell>{book.first_publish_year}</TableCell>
+								<TableCell>{book.number_of_pages_median || "-"}</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			</div>
+			<div className="mt-5 flex items-center justify-between">
+				<Button
+					variant="outline"
+					onClick={handlePreviousClick}
+					disabled={currentPage <= 1 || isLoading}
+				>
+					Previous
+				</Button>
+				<span>Page {currentPage}</span>
+				<Button
+					variant="outline"
+					onClick={handleNextClick}
+					disabled={
+						currentPage >= Math.ceil(totalResults / resultsPerPage) || isLoading
+					}
+				>
+					Next
+				</Button>
 			</div>
 		</div>
 	)
